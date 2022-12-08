fb5fdd51402ee7f6394200d38aed673aa3a606ff
Kwamzy99 <Amenyajoy99@gmail.com>
2022-12-08 05:02:10 -0800

    Hello push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,33 +1,3 @@
 #!/bin/bash
-int main(void) {puts("Programming is like building a multilingual puzzle\n");return (0); }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(void) {puts("Programming is like building a multilingual puzzle\n");return (0);}
 #include <stdio.h>
