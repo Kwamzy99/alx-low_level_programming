80fa7d59e3c2274785878f3a2c72f0db87b2e83b
Kwamzy99 <Amenyajoy99@gmail.com>
2022-12-08 08:31:13 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,5 +4,6 @@
  * Return:Success
  */
 int main(void)
-{printf ("with proper grammar, but the outcome is a piece of art,\n"); return (0);
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
