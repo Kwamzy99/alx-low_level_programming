b55a728b0a795ed3278e7c242a5ea0cec93b6bae
Kwamzy99 <Amenyajoy99@gmail.com>
2022-12-08 08:34:54 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - entry
- * Return: Success
+ * main-entry
+ * Return:Success
  */
 int main(void) 
-{
-puts("Programming is like building a multilingual puzzle\n"); 
+{puts("Programming is like building a multilingual puzzle"); 
 return (0); 	
 }
