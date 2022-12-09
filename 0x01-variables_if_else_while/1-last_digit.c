29ae9263a440b7b03b4450e81a0bde3f4dcd6bbf
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-09 04:04:44 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	printf("%d and is 0\n", n);
 } else if (n < 5)
 {
-	printf("%d and is less than 6 and not 0\n, n);
+	printf("%d and is less than 6 and not 0\n", n);
 }
 return (0);
 }
