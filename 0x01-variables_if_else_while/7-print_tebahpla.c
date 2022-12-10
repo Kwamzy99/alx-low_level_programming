529ee3c0681a9d70879f92c522c343dad358be1e
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-10 04:42:12 -0800

    Smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -a program that prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int (main)void
+{
+	char letter;
+
+	for (letter = 'a' ; letter = 'z'; letter--)
+
+		putchar(letter);
+	putchar('\n')
+	return (0);
+}
