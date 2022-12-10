a5def16e5e3c9e5f469c358521b80f58c0164eb2
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-10 00:25:47 -0800

    Alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0
  */
-int (maiin) ; void
+int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	({
+	{
 		if (letter != 'e' && letter != 'q')
 
 			putchar(letter);
