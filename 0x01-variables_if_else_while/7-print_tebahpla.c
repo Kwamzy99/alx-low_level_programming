326bd0a659597b1fda88bd685845a0a27bad176d
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-13 01:05:30 -0800

    Reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -a program that prints the lowercase alphabet in reverse
+ * main - a program that prints the lowercase alphabet in reverse
  * Return: 0
  */
 int main(void)
 {
 	char letter;
 
-	for (letter = 'z' ; letter = 'a'; letter--)
+	for (letter = 'z' ; letter >= 'a'; letter--)
 
 		putchar(letter);
 	putchar('\n');
