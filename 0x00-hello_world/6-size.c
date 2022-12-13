7a27e13f026292628638329935d48de0bb2f9d69
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-12 23:51:28 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types 
  * Return: 0
  */
 int main(void)
@@ -12,5 +12,5 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }
