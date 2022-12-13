904f606cd5c72461773076c7960a9d7fbc9f18d8
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-13 03:35:32 -0800

    10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  prints 10 times the alphabet, in lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar((letter x 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
