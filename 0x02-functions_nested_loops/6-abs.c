1ea52fb219cd53a84c96917ea092248453a29fb8
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-14 03:50:37 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * Return: 0
  */
 int _abs(int)
 {
 	int num;
 
-	if (num > 0)
+	if (num >= 0)
 		printf(num);
 	else 
-		return ();
+		return (-num);
 }
