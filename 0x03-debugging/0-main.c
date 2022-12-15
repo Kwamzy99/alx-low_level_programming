229306b06d1608fa541da4bf3fe89e2fe6a13268
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-14 23:12:50 -0800

    Main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -  gives the correct output when given a case of 0
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
