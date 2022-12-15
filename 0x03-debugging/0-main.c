b00064cd4530c94df9c4001a413352c57eaec18f
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-15 06:59:40 -0800

    Main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is +/-
+ * positive_or_negative - tests function that prints if integer is +/-
  * Return: 0
  */
 int main(void)
