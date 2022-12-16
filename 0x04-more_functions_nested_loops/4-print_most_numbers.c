d845fdeedee2917049b49401de81caf5ddf8980e
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-16 04:43:57 -0800

    2/4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,10 @@ void print_most_numbers(void)
 
 	for (c = '0' ; c <= '9'; c++)
 	{
-	if (c == '2' && c == '4')
-	putchar(c);
+	if ((c == 2) || (c == 4))
+		continue;
+	else
+		_putchar(c + '0');
 	}
-	putchar('\n');
+	_putchar('\n');
 }
