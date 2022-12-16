babb235bb50edc7f030c796fb4644d142e04cea3
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-16 05:52:46 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers
+ * _putchar only 3 times
+ * Return: 0-14 x10
+ */
+void more_numbers(void)
+{
+	int a, b;
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
