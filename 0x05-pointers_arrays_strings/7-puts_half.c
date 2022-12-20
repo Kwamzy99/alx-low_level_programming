d767a3c0fe5f0cdf899db78af9add25c23b50822
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-20 06:22:55 -0800

    Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main"
+/**
+ * puts_half - prints half of a string
+ * @str: string 
+ * Return: string
+ */
+void puts_half(char *str)
+{
+	int a, b, l;
+
+	l =  0;
+	
+	for (a = 0; str[a] != '\0'; a++)
+		l++;
+
+	b = (l / 2);
+
+	if ((l % 2) == 1)
+		b = ((l + 1) / 2);
+
+	for (a = b; str[a] != '\0'; a++)
+		_putchar(str[n]);
+	_putchar('\n');
+}
+
