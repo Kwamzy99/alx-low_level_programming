1f9b95fb91a22066f58c13bea135e1fb8e1806a0
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-20 00:39:39 -0800

    Rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, n;
 
-	for (i = 0; s[i] != '\0'; i++)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
 
-	for (i = 1; s[i] != '\0'; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
