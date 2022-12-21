2d1956941ba3681f91d98c7a281ff85bb1c6d868
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-21 06:02:21 -0800

    cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat -  concatenates two strings
+ * @dest: destination of string
+ * @src: string to be copies
+ * @n: length
+ * Return: two strings
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+	
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; b < n; b++)
+	{
+		dest [a + b] = src[b];
+		if (src[b] == '\0')
+			b = n;
+	}
+	return (dest);
+}
