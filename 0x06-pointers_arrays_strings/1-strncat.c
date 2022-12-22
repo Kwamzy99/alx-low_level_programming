789b2c65fbe07824df33ca02b0bbea85e41a296d
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-21 23:03:32 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
-	
 	for (a = 0; dest[a] != '\0'; a++)
 	{
 	}
 	for (b = 0; b < n; b++)
 	{
-		dest [a + b] = src[b];
+		dest[a + b] = src[b];
 		if (src[b] == '\0')
 			b = n;
 	}
