e5bc58edff732b9eaf8011184a9894a51b5550a7
Kwamzy99 <amenyajoy99@gmail.com>
2022-12-22 05:09:19 -0800

    Cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @i: integer
+ * Return: capitalizes the words
+ */
+char *cap_string(char *i)
+{
+	int a, b;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '!', '?',
+	       	'"', '(', ')', '{', '}', ' ', '\t', '\n'};
+
+	for (a = 0; i[a] != '\0'; a++)
+	{
+		if (i[a] >= 'a' && i[a] <= 'z')
+		{
+			i[a] = i[a] - cap;
+		}
+
+		cap = 0;
+
+		for (b = 0; b <= 12; b++)
+		{
+			if (i[a] == separators[b])
+			{
+				b = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (n);
+}
