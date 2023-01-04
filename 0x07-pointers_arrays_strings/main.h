96687402835345fafe021252d684bfed4858e73c
Kwamzy99 <amenyajoy99@gmail.com>
2023-01-03 21:40:29 -0800

    Main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,8 @@
 #ifndef main_h
 #define main_h
-/**
- * main - Header file containing all prototypes
- */
-int _putchar(char);
+
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
