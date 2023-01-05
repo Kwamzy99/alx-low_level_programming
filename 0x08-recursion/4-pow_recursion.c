19187a84a2dee8a219970a166f9b012baaa46013
Kwamzy99 <amenyajoy99@gmail.com>
2023-01-05 06:16:25 -0800

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,18 +3,14 @@
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: int
  * @y: int
- * Return: Always 0 
+ * Return: Always 0
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	else if (y != 0)
-		return (x * _pow_recursion(x, y - 1));
-	else
-	{
+	else if (y == 0)
 		return (1);
-	}
+	else
+		return (n * factorial(n - 1));
 }
