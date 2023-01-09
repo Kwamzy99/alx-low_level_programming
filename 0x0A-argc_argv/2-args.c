a3776c067509ecb454ea3b6065568509bb25eae5
Kwamzy99 <amenyajoy99@gmail.com>
2023-01-09 00:33:16 -0800

    2args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all arguments it receives
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
